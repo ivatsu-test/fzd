49932a92c30fe0bf34003c08e612a00018e49091
IvaTsu <ivan.tsud@gmail.com>
2025-03-17 22:19:55 +0100

    feat: init basic camera setup

--- app/camera.tsx
+++ app/camera.tsx
@@ -1,13 +1,58 @@
-import { Text } from 'react-native';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
 import MainView from '../components/main-view';
 import NavLink from '../components/nav-link';
 import route from '../constants/route';
 
 export default function CameraPage() {
+    const device = useCameraDevice('back')
+    const { hasPermission, requestPermission } = useCameraPermission()
+
+    useEffect(() => {
+        if (!hasPermission) {
+            requestPermission();
+        }
+    }, [])
+
+    if (!hasPermission) {
+        return (
+            <MainView>
+                <Text>No Camera Permision Granted!</Text>
+                <NavLink href={route.home}>Home</NavLink>
+            </MainView>
+        )
+    }
+
+    if (device == null) {
+        return (
+            <MainView>
+                <Text>No Camera Device!</Text>
+                <NavLink href={route.home}>Home</NavLink>
+            </MainView>
+        )
+    }
+
     return (
-        <MainView>
-            <Text>Camera Page</Text>
+        <View style={styles.container}>
             <NavLink href={route.home}>Home</NavLink>
-        </MainView>
-    );
+            <Camera
+                style={styles.camera}
+                device={device}
+                isActive={true}
+            />
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        margin: 16
+    },
+    camera: {
+        flex: 1,
+        marginTop: 16
+    }
+});
